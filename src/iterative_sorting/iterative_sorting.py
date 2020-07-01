09dafda28ef683aefaaaf0d4608a4a8493749d88
cladams0203 <cladams0203@gmail.com>
2020-07-01 16:01:13 -0400

    finished selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,11 +7,15 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
+        for a in range(cur_index, len(arr)):
+            if(arr[a] < arr[smallest_index]):
+                smallest_index = a
 
         # TO-DO: swap
         # Your code here
-
+        temp = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = temp
     return arr
 
 
